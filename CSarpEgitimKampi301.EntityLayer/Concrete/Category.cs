138c033de7e879b7fc1492e975a813fe06fb5cac
Code Club <metin.beci@idepex.com>
2024-11-21 13:03:37 +0300

    Add project files.

---
+++ CSarpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSarpEgitimKampi301.EntityLayer.Concrete
+{
+    /*
+        - field-Variable-property
+    */
+    
+    //int x; // Field (Clasın içine tanımlanırsa)
+    //public int y { get; set; }  // Property 
+    //void test() // Variable (metot içinde tanımlanırsa değişken )
+    //{
+    //    int z;
+    //}
+    public class Category
+    {
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+
+    }
+}
